032489ed8d157fdf6464c6e6e6525c5fc4c0455d
Roman Naumov <romas.naumov@gmail.com>
2016-12-25 00:14:13 +0300

    Подключение внешнего файла script.js

---
+++ js/script.js
@@ -0,0 +1,42 @@
+var navMain = document.querySelector('.navigation');
+var navToggle = document.querySelector('.navigation__toggle');
+
+var basket = document.querySelector(".catalog-item__basket");
+var windowModal = document.querySelector(".modal");
+var layer = document.querySelector(".modal-overlay");
+
+navMain.classList.remove('navigation--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('navigation--closed')) {
+    navMain.classList.remove('navigation--closed');
+    navMain.classList.add('navigation--opened');
+  } else {
+    navMain.classList.add('navigation--closed');
+    navMain.classList.remove('navigation--opened');
+  }
+});
+
+if(basket) {
+  basket.addEventListener("click", function(event) {
+    event.preventDefault();
+    windowModal.classList.add("modal--show");
+    layer.classList.add("modal-overlay--show");
+  });
+}
+
+layer.addEventListener("click", function(event) {
+  windowModal.classList.remove("modal--show");
+  layer.classList.remove("modal-overlay--show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (windowModal.classList.contains("modal--show")) {
+      windowModal.classList.remove("modal--show");
+    }
+    if (layer.classList.contains("modal-overlay--show")) {
+      layer.classList.remove("modal-overlay--show");
+    }
+  }
+});
